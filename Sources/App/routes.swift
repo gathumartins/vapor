9b1dca94732055221d4eb0cdbb722001c710b586
gathumartins <gathumartins@gmail.com>
2018-11-24 09:58:57 +0300

    first commit

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -11,7 +11,9 @@ public func routes(_ router: Router) throws {
     router.get("hello") { req in
         return "Hello, world!"
     }
-
+    router.get("guest") { req in
+        return "Hello, guest!"
+    }
     // Example of configuring a controller
     let todoController = TodoController()
     router.get("todos", use: todoController.index)
